8b796b7ee3af798097a19fbe46898f10436d4a27
Mobius Dev <gabriel.machado.cg@gmail.com>
2023-02-27 03:11:25 -0300

    island manager & groups configured

---
+++ MKS-PirateBattle/Assets/Scripts/IslandsManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.AI;
+
+public class IslandsManager : MonoBehaviour
+{
+    public GameObject[] islandGroups;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnGroup(Random.Range(0, islandGroups.Length));
+    }
+
+    private void SpawnGroup(int index)
+    {
+        GameObject groupClone = Instantiate(islandGroups[index], transform);
+        NavMeshBuilder.ClearAllNavMeshes();
+        NavMeshBuilder.BuildNavMesh();
+    }
+}
